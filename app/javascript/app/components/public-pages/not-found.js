5536a00f57f9a2e9941d0f217b11e97c02aa51ce
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-06-05 07:55:08 -0700

    <Hello name="React" />

--- app/javascript/app/components/public-pages/not-found.js
+++ app/javascript/app/components/public-pages/not-found.js
@@ -3,11 +3,10 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h1>Not found</h1>
-      <p>lorem ipsum...</p>
+      <h1>Page Not found</h1>
+      <p>Our deepest apologies, this is probably our fault. Please try a different URL.</p>
     </div>
   );
 };
 
 export default NotFound;
-
